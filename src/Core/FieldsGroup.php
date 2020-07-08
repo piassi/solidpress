ccd019ead724ed01076937954ab03455581da0dc
Thales Piassi <thales.piassi.nunes@gmail.com>
2020-07-08 17:46:05 -0300

    Move fields from $args array to class props

--- src/Core/FieldsGroup.php
+++ src/Core/FieldsGroup.php
@@ -14,6 +14,8 @@ abstract class FieldsGroup implements Registrable {
 	 * @var array
 	 */
 	protected $args = [];
+	protected $fields = [];
+
 
 	/**
 	 * Get group fields args and register them with acf_add_local_field_group function.
@@ -25,10 +27,26 @@ abstract class FieldsGroup implements Registrable {
 	public function register(): void {
 		$args = $this->args;
 		$args['fields'] = Field::get_fields_args(
-			$this->args['fields'],
+			$this->fields,
 			$this->args['key']
 		);
 
 		acf_add_local_field_group($args);
+	}
+
+	public function set_fields(array $fields): void
+	{
+		$this->fields = $fields;
+	}
+
+	public function get_fields_values(): array
+	{
+		$fields_values = [];
+
+		foreach($this->fields as $field_key => $field){
+			$fields_values[$field_key] = $field->get_value($field_key, $this->args) ?? [];
+		}
+
+		return $fields_values;
 	}
 }
