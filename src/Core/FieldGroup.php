c8474ac0aeac4059e113ad390d5e1e67404b5e45
Thales Piassi <thales.piassi.nunes@gmail.com>
2020-07-17 09:51:42 -0300

    Fixes typo in Fields Group class

--- src/Core/FieldGroup.php
+++ src/Core/FieldGroup.php
@@ -5,9 +5,9 @@ namespace SolidPress\Core;
 use SolidPress\Interfaces\Registrable;
 
 /**
- * Creates a new interface to ACF Field Groups
+ * Creates a new interface to ACF Field Group
  */
-abstract class FieldsGroup implements Registrable {
+abstract class FieldGroup implements Registrable {
 	/**
 	 * Field group register arguments
 	 *
@@ -39,7 +39,7 @@ abstract class FieldsGroup implements Registrable {
 		$this->fields = $fields;
 	}
 
-	public static function get_values(FieldsGroup $fields_group, array $fields_to_get = []): array
+	public static function get_values(FieldGroup $fields_group, array $fields_to_get = []): array
 	{
 		if(!$fields_to_get){
 			$fields_to_get = array_keys($fields_group->fields);
