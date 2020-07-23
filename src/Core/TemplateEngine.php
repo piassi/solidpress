5f06f232d56978fafdcc32a77bc30c87e7b1fafa
Thales Piassi <thales.piassi.nunes@gmail.com>
2020-07-23 17:42:15 -0300

    Add rule to enqueue custom post type

--- src/Core/TemplateEngine.php
+++ src/Core/TemplateEngine.php
@@ -5,12 +5,13 @@ namespace SolidPress\Core;
 use SolidPress\Interfaces\Renderable;
 
 abstract class TemplateEngine {
+
 	public $engine;
 
-	abstract public function render(string $template, array $params): void;
+	abstract public function render( string $template, array $params = array()): void;
 	abstract public function render_to_string(
 		string $template,
-		array $params
+		array $params = array()
 	): string;
-	abstract public function render_object(Renderable $template): string;
+	abstract public function render_object( Renderable $template): string;
 }
