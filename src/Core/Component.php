5bacaaaad15757428d2ba61954d0e39f3d03e8d9
Thales Piassi <thales.piassi.nunes@gmail.com>
2023-04-05 10:42:53 -0300

    feat: add template method

--- src/Core/Component.php
+++ src/Core/Component.php
@@ -20,10 +20,20 @@ abstract class Component implements Renderable {
 
 	public function __toString(): string {
 		try {
-			return Theme::get_instance()->template_engine->render_object( $this );
-		} catch ( \Throwable $e ) {
+			ob_start();
+			if ( $this->template ) {
+				return Theme::get_instance()->template_engine->render_object( $this );
+			}
+
+			$this->template( array_merge( $this->props, $this->get_props() ) );
+			return ob_get_clean();
+		} catch (\Throwable $e) {
 			return $e->getMessage();
 		}
+	}
+
+	public function template( array $props ): void {
+		throw new \Exception( 'Template method not defined' );
 	}
 
 	public function get_props(): array {
